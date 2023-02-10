54d0ab570a2d4f5fb5a3b977e2ae0f6385d0502f
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2023-02-10 10:00:09 -0500

    setup main screen

--- app/src/main/java/com/kazumaproject/markdownnote/MainActivity.kt
+++ app/src/main/java/com/kazumaproject/markdownnote/MainActivity.kt
@@ -2,10 +2,13 @@ package com.kazumaproject.markdownnote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
