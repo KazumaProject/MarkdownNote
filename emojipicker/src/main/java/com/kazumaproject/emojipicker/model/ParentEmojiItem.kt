ff2081595cc7cee8a0c43b65e90eb658cdb12810
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2023-02-12 19:33:56 -0500

    Update ParentEmojiItem.kt

--- emojipicker/src/main/java/com/kazumaproject/emojipicker/model/ParentEmojiItem.kt
+++ emojipicker/src/main/java/com/kazumaproject/emojipicker/model/ParentEmojiItem.kt
@@ -1,7 +1,5 @@
 package com.kazumaproject.emojipicker.model
 
-import com.kazumaproject.emojipicker.model.Emoji
-
 data class ParentEmojiItem(
     val patentTitle: String,
     val childItemList: List<Emoji>
