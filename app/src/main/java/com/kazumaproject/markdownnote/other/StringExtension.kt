63e7e6ab070d018cf928c66f0ccf7d2e9492fb78
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2023-02-20 04:09:41 -0500

    get first line for note title

--- app/src/main/java/com/kazumaproject/markdownnote/other/StringExtension.kt
+++ app/src/main/java/com/kazumaproject/markdownnote/other/StringExtension.kt
@@ -1,6 +1,6 @@
 package com.kazumaproject.markdownnote.other
 
-fun String.getTitleFromNote(): String?{
-    val title = this.split("[\r\n]+")
+fun String.getTitleFromNote(): String {
+    val title = this.split("\r?\n|\r".toRegex()).toTypedArray()
     return if (title.isEmpty()) this else title[0]
 }
